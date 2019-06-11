0075b1d1ad97f4b83eaad2a74690b4e8ef790623
Sindhumeenakshi <51654472+Sindhumeenakshi@users.noreply.github.com>
2019-06-11 20:44:47 +0530

    Create vowel.py

---
+++ codekata/vowel.py
@@ -0,0 +1,10 @@
+ch=input()
+list1=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+list2=["a","e","i","o","u"]
+if((ch in list1)==True):
+  if(ch in list2):
+    print("Vowel")
+  else:
+    print("consonant")
+else:
+  print("invalid")
