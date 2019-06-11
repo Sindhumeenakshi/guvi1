449979802dbdfd137838a30f120833c4b100072c
Sindhumeenakshi <51654472+Sindhumeenakshi@users.noreply.github.com>
2019-06-11 20:52:23 +0530

    Create alphabetornot.py

---
+++ codekata/alphabetornot.py
@@ -0,0 +1,7 @@
+ch=input()
+list1=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+if((ch in list1)==True):
+  print("Alpabet")
+else:
+  print("No")
+
