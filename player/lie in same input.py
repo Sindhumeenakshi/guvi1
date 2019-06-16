209f79ebe55b988fe579062ea992f92744ab076d
Sindhumeenakshi <51654472+Sindhumeenakshi@users.noreply.github.com>
2019-06-16 18:38:28 +0530

    Update lie in same input.py

--- player/lie in same input.py
+++ player/lie in same input.py
@@ -1,7 +1,9 @@
-d=input().split()
-e=input().split()
-f=input().split()
-if(d[0]==e[0]==f[0] or (d[1]==e[1]==f[1] or (d[0]==d[1] and e[0]==e[1] and f[0]==f[1])):
-    print('yes')
+s,r=map(int,input().split())
+a,b=map(int,input().split())
+q,r=map(int,input().split())
+if s==r or a==b or q==r:
+    print("yes")
+elif s==a==q or r==b==r:
+    print("yes")
 else:
-    print('no')
+    print("no")
