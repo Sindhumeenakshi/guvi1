ae2c13efe98d356828fc43fb92f033239d0d37e0
Sindhumeenakshi <51654472+Sindhumeenakshi@users.noreply.github.com>
2019-06-16 19:51:45 +0530

    Create primefactor in sorted.py

---
+++ player/primefactor in sorted.py
@@ -0,0 +1,13 @@
+b=int(input())
+m=[]
+c=0
+for j in range(2,b+1):
+  if(b%i)==0:
+    m.append(j)
+for j in m:
+  c=0
+  for k in range(1,j+1,1):
+    if (j%k)==0:
+      c=c+1
+  if c==2:
+    print (j,end=" ")
