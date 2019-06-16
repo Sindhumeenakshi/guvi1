167553addd163c913462c6818e66d33197760e08
Sindhumeenakshi <51654472+Sindhumeenakshi@users.noreply.github.com>
2019-06-16 19:39:48 +0530

    Update not repeat.py

--- player/not repeat.py
+++ player/not repeat.py
@@ -1,8 +1,8 @@
 nu = int(raw_input())
 lis = map(int,raw_input().split())
 count = []
-for i in range(len(lis)):
-  count.append(lis.count(lis[i]))
-for i in range(len(count)):
-  if count[i] is 1:
-    print lis[i]
+for j in range(len(lis)):
+  count.append(lis.count(lis[j]))
+for j in range(len(count)):
+  if count[j] is 1:
+    print lis[j]
