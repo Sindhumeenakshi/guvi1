b1c4b83af86d5f4a814824a31be8a1c674b57245
Sindhumeenakshi <51654472+Sindhumeenakshi@users.noreply.github.com>
2019-06-16 18:58:31 +0530

    Create max after array insertion.py

---
+++ player/max after array insertion.py
@@ -0,0 +1,9 @@
+s,r=map(int,input().split()) 
+a=input()
+n=list(map(int,input().split()))
+g=list(map(int,input().split()))
+b=[]
+for i in range(r):
+    m.append(g[i])
+    b.append(max(n))
+print(*b)
